5bc051a9a028fe7ab1d05f2e7fcef4c1ba64071a
aliyah-a <78117261+aliyah-a@users.noreply.github.com>
2021-01-27 17:45:46 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/****************************
+* Hello Java
+*****************************
+* Aliyah Allen
+* 1/27/21
+* CMSC 255-003
+*****************************/
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Aliyah Allen");
+		System.out.println("Hello Java");
+		System.out.println("CMSC Lab 1");
+		System.out.println("CMCS 255-003");
+		System.out.println();
+		}
+	}
